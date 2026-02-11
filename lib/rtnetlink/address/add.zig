b5298eaeb6efb5c27e68c3697cec47bcf901536f
Trim Bresilla <trim.bresilla@gmail.com>
2026-02-11 19:07:44 +0100

    fix: correct ipv4 broadcast calculation endianness

--- lib/rtnetlink/address/add.zig
+++ lib/rtnetlink/address/add.zig
@@ -21,17 +21,31 @@ pub fn init(allocator: std.mem.Allocator, nl: *RtNetLink, options: Options) Addr
 }
 
 fn applyOptions(self: *AddrAdd) !void {
+    if (self.opts.prefix_len > 32) return error.InvalidPrefixLength;
+
     self.msg.msg.hdr.index = @intCast(self.opts.index);
     self.msg.msg.hdr.prefix_len = self.opts.prefix_len;
     try self.msg.addAttr(.{ .address = self.opts.addr });
     try self.msg.addAttr(.{ .local = self.opts.addr });
 
-    if (self.opts.prefix_len == 32) {
-        try self.msg.addAttr(.{ .broadcast = self.opts.addr });
-    } else {
-        const brd = (@as(u32, 0xffff_ffff) >> @intCast(self.opts.prefix_len)) | std.mem.bytesAsValue(u32, &self.opts.addr).*;
-        try self.msg.addAttr(.{ .broadcast = std.mem.toBytes(brd) });
-    }
+    try self.msg.addAttr(.{ .broadcast = computeBroadcast(self.opts.addr, self.opts.prefix_len) });
+}
+
+fn computeBroadcast(addr: [4]u8, prefix_len: u8) [4]u8 {
+    if (prefix_len >= 32) return addr;
+
+    const addr_be = std.mem.readInt(u32, &addr, .big);
+    const netmask: u32 = if (prefix_len == 0)
+        0
+    else blk: {
+        const host_bits: u5 = @intCast(32 - prefix_len);
+        break :blk @as(u32, 0xffff_ffff) << host_bits;
+    };
+    const broadcast = addr_be | ~netmask;
+
+    var out: [4]u8 = undefined;
+    std.mem.writeInt(u32, &out, broadcast, .big);
+    return out;
 }
 
 pub fn exec(self: *AddrAdd) !void {
@@ -42,4 +56,16 @@ pub fn exec(self: *AddrAdd) !void {
 
     try self.nl.send(data);
     return self.nl.recv_ack();
+}
+
+test "computeBroadcast keeps /32 unchanged" {
+    try std.testing.expectEqual([4]u8{ 10, 1, 2, 3 }, computeBroadcast(.{ 10, 1, 2, 3 }, 32));
+}
+
+test "computeBroadcast computes /24 broadcast" {
+    try std.testing.expectEqual([4]u8{ 192, 168, 12, 255 }, computeBroadcast(.{ 192, 168, 12, 34 }, 24));
+}
+
+test "computeBroadcast computes /0 broadcast" {
+    try std.testing.expectEqual([4]u8{ 255, 255, 255, 255 }, computeBroadcast(.{ 10, 20, 30, 40 }, 0));
 }
